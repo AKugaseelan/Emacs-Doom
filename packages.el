8251ca928f829819f976841047e5f7a4c24a1da9
Athiraiyan Kugaseelan <athiraiyan.kugasselan@outlook.com>
2020-05-29 01:52:12 +0200

    Java-LSP almost done

--- packages.el
+++ packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+(package! lsp-java)
+(package! dap-mode)
+(package! origami)
